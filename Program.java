05fae6a5600f60306d2e379c96cbe8249273e340
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-01 15:11:50 +0900

    자바 프로그래밍
    생활코딩 자바 독학

---
+++ Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Program = 자동화!!
+		//1 - 3까지 순차적으로 진행되도록 하는 프로그래밍.
+		// ex) 1번이 끝나는 시간이 1년이라고 하면 사람이 기다리기에는 시간낭비이므로 순차적으로 진행하게 해주는 편리한 기능
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+
+	}
+
+}

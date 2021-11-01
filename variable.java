4824a13eb849064c546f22b944a428341c8a8916
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-01 15:08:08 +0900

    변수

---
+++ variable.java
@@ -0,0 +1,17 @@
+//변수
+public class variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // int = 정수 ex) ..., -3, -2, -1, 0,  1, 2, 3, ...
+		System.out.println(a);
+		
+		double b = 1.3; // double = 실수
+		System.out.println(b);
+		
+		String c = "Hello!!";
+		System.out.println(c);
+		
+	}
+
+}

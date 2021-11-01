e529bd7aa286a9ecafc84af619cc0ae32d5bde5c
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-01 15:07:33 +0900

    변수의 효용
    생활코딩 자바 수업

---
+++ Letter.java
@@ -0,0 +1,19 @@
+//변수의 효용
+//예를 들어 변수의 이름이 vat(부가가치세)면 다른 사람들이 봤을 때 누구든지 알 수 있는것처럼 효율이 좋다!
+
+public class Letter {
+
+	public static void main(String[] args) {
+
+		String name = "hanmaesil";
+		
+		System.out.println("hello, "+name+" blah blah blah "+name+" bye");
+				
+		
+		double VAT = 1.3;
+		System.out.println(6000*VAT);
+		
+
+	}
+
+}
